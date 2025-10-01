5cfa2aea0004df99a72d4fabe8593047a8691f1c
Nitin Prakash <nitin247@outlook.com>
2025-10-01 10:03:41 +0530

    run.sh removed

--- run.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "./wp-admin" ]; then
-	wp core download
-fi
-
-if ! $(wp core is-installed); then
-	wp core install --url="http://wp-studio.code" --title="WP Studio" --admin_user="admin" --admin_password="password" --admin_email="admin@wp-studio.code" --skip-email
-fi
-
-
